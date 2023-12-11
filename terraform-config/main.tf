3631c2370cec48e389dec04a73ce48bca5234436
XateroX <dan.lostarkgames@gmail.com>
2023-12-11 16:46:56 +0000

    updated deployment

--- terraform-config/main.tf
+++ terraform-config/main.tf
@@ -1,5 +1,41 @@
 locals {
   branches = ["dev", "test", "prod"]
+
+  client_urls = {
+    dev  = data.google_secret_manager_secret_version.client_url_dev.secret_data
+    test = data.google_secret_manager_secret_version.client_url_test.secret_data
+    prod = data.google_secret_manager_secret_version.client_url_prod.secret_data
+  }
+
+  server_urls = {
+    dev  = data.google_secret_manager_secret_version.server_url_dev.secret_data
+    test = data.google_secret_manager_secret_version.server_url_test.secret_data
+    prod = data.google_secret_manager_secret_version.server_url_prod.secret_data
+  }
+}
+
+data "google_secret_manager_secret_version" "client_url_dev" {
+  secret_id = "client_url_dev"
+}
+
+data "google_secret_manager_secret_version" "server_url_dev" {
+  secret_id = "server_url_dev"
+}
+
+data "google_secret_manager_secret_version" "client_url_test" {
+  secret_id = "client_url_test"
+}
+
+data "google_secret_manager_secret_version" "server_url_test" {
+  secret_id = "server_url_test"
+}
+
+data "google_secret_manager_secret_version" "client_url_prod" {
+  secret_id = "client_url_prod"
+}
+
+data "google_secret_manager_secret_version" "server_url_prod" {
+  secret_id = "server_url_prod"
 }
 
 provider "google" {
@@ -42,6 +78,16 @@ resource "google_cloud_run_service" "server_default" {
     spec {
       containers {
         image = "europe-west2-docker.pkg.dev/dp-rct-dev/online-game/image:${local.branches[count.index]}"
+
+        env {
+          name  = "CLIENT_BASE_URL"
+          value = local.client_urls[local.branches[count.index]]
+        }
+
+        env {
+          name  = "VITE_SERVER_URL"
+          value = local.server_urls[local.branches[count.index]]
+        }
       }
     }
   }
