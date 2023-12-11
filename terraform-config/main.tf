5e3dd0548ae6f081ed1955a9cfb0c09affb6620b
XateroX <dan.lostarkgames@gmail.com>
2023-12-11 13:43:42 +0000

    initial commit

---
+++ terraform-config/main.tf
@@ -0,0 +1,40 @@
+locals {
+  branches = ["dev", "test", "prod"]
+}
+
+provider "google" {
+  project = "dp-rct-dev"
+  region  = "europe-west2"
+}
+
+resource "google_cloudbuild_trigger" "default" {
+  count = length(local.branches)
+  name = "trigger-${local.branches[count.index]}"
+  github {
+    owner = "XateroX"
+    name  = "OnlineGame"
+    push {
+      branch = "^${local.branches[count.index]}$"
+    }
+  }
+  filename = "cloudbuild.yaml"
+}
+
+resource "google_cloud_run_service" "default" {
+  count    = length(local.branches)
+  name     = "image-${local.branches[count.index]}"
+  location = "europe-west2"
+
+  template {
+    spec {
+      containers {
+        image = "europe-west2-docker.pkg.dev/dp-rct-dev/online-game/image:${local.branches[count.index]}"
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
