a3ef1f868a45e4270f95043108f217044006f41c
XateroX <dan.lostarkgames@gmail.com>
2023-12-11 15:20:53 +0000

    Add Dockerfile and Vite configuration for online game

--- terraform-config/main.tf
+++ terraform-config/main.tf
@@ -7,7 +7,7 @@ provider "google" {
   region  = "europe-west2"
 }
 
-resource "google_cloudbuild_trigger" "default" {
+resource "google_cloudbuild_trigger" "server_default" {
   count = length(local.branches)
   name = "trigger-${local.branches[count.index]}"
   github {
@@ -20,7 +20,20 @@ resource "google_cloudbuild_trigger" "default" {
   filename = "online-game-server/cloudbuild.yaml"
 }
 
-resource "google_cloud_run_service" "default" {
+resource "google_cloudbuild_trigger" "client_default" {
+  count = length(local.branches)
+  name = "trigger-${local.branches[count.index]}"
+  github {
+    owner = "XateroX"
+    name  = "OnlineGame"
+    push {
+      branch = "^${local.branches[count.index]}$"
+    }
+  }
+  filename = "online-game/cloudbuild.yaml"
+}
+
+resource "google_cloud_run_service" "server_default" {
   count    = length(local.branches)
   name     = "image-${local.branches[count.index]}"
   location = "europe-west2"
@@ -29,6 +42,25 @@ resource "google_cloud_run_service" "default" {
     spec {
       containers {
         image = "europe-west2-docker.pkg.dev/dp-rct-dev/online-game/image:${local.branches[count.index]}"
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
+
+resource "google_cloud_run_service" "client_default" {
+  count    = length(local.branches)
+  name     = "client-image-${local.branches[count.index]}"
+  location = "europe-west2"
+
+  template {
+    spec {
+      containers {
+        image = "europe-west2-docker.pkg.dev/dp-rct-dev/online-game/client-image:${local.branches[count.index]}"
       }
     }
   }
