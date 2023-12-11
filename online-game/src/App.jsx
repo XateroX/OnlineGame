3e50130d480a053dfcb0144cab991bc4ce0081c8
XateroX <dan.lostarkgames@gmail.com>
2023-12-11 17:07:00 +0000

    added additional debugging

--- online-game/src/App.jsx
+++ online-game/src/App.jsx
@@ -8,11 +8,13 @@ function App() {
   const apiClient = new Api()
 
   const [exampleText, setExampleText] = useState('getting text...')
+  const [exampleText2, setExampleText2] = useState({})
 
   useEffect(() => {
     const fetchInitData = async () => {
       const res = await apiClient.getExample()
       setExampleText(res.data.exampleText)
+      setExampleText2(res.data)
       console.log(res)
     }
     fetchInitData()
@@ -21,6 +23,7 @@ function App() {
   return (
     <APIContext.Provider value={{ apiClient }}>
       <p>{exampleText}</p>
+      <p>{exampleText2}</p>
     </APIContext.Provider>
   )
 }
