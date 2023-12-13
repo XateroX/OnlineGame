513680144b8924ac387a0572e1139241e541029d
XateroX <dan.lostarkgames@gmail.com>
2023-12-13 00:25:38 +0000

    Update server and client configurations and added basic heartbeat from client and connection managing from server

--- online-game-server/server.js
+++ online-game-server/server.js
@@ -28,12 +28,60 @@ const server = http.createServer(app);
 
 const io = socketIO(server, { cors: corsOptions });
 
+let gameData = {
+    time: 0,
+    players: {},
+    // other game data...
+};
+
+
 io.on('connection', (socket) => {
     console.log('a user connected:', socket.handshake.headers.referer);
-    socket.on('disconnect', () => {
-        console.log("user disconnected");
+
+    // When a player connects, add them to the game data
+    gameData.players[socket.id] = {
+        "lastHeartbeat": Date.now(),
+    };
+
+    // use heartbeat to check if clients are still connected
+    socket.on('heartbeat', () => {
+        console.log('heartbeat from: ', socket.id);
+        gameData.players[socket.id]["lastHeartbeat"] = Date.now();
     });
 });
+
+
+
+setInterval(() => {
+    // Update the game time
+    gameData.time++;
+
+    // Update other game data...
+
+    // Emit the updated game data to all connected clients
+    io.emit('gameData', gameData);
+    console.log('just emitted gameData with player count: ', Object.keys(gameData.players).length);
+
+    // console log the player id list
+    console.log('player id list: ', Object.keys(gameData.players));
+
+    // all players whose last heartbeat was more than 5 seconds ago are disconnected
+    Object.keys(gameData.players).forEach((playerId) => {
+        if (Date.now() - gameData.players[playerId].lastHeartbeat > 5000) {
+            // disconnect the player
+            try {
+                io.sockets.connected[playerId].disconnect();
+            } catch (error) {
+                console.log('error: ', error);
+            }
+
+            delete gameData.players[playerId];
+            console.log('deleted player: ', playerId);
+            io.emit('playerDisconnected', playerId);
+
+        }
+    });
+}, 1000);
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
--- online-game/src/App.jsx
+++ online-game/src/App.jsx
@@ -1,28 +1,80 @@
 import { useState, createContext, useEffect } from 'react'
 import './App.css'
 import Api from './api'
+import io from 'socket.io-client'
 
 export const APIContext = createContext([])
 
 function App() {
   const apiClient = new Api()
 
-  const [exampleText, setExampleText] = useState('getting text...')
-  const [serverurl, setServerurl] = useState(`${import.meta.env.VITE_SERVER_URL}`)
+  const [exampleText, setExampleText] = useState('getting text...');
+  const [serverurl, setServerurl] = useState(`${import.meta.env.VITE_SERVER_URL}`);
+  const [playerPos, setPlayerPos] = useState({ x: 0, y: 0 });
+  const [socketIdList, setSocketIdList] = useState([]);
 
   useEffect(() => {
-    const fetchInitData = async () => {
-      const res = await apiClient.getExample()
-      setExampleText(res.data.exampleText)
-      console.log(res)
+    try {
+      const socket = io(serverurl);
+
+      // a heartbeat function to keep the connection alive
+      setInterval(() => {
+        socket.emit('heartbeat', {});
+        console.log('heartbeat');
+        console.log(socket.id)
+
+        // if the socket.id isnt in the list, add it
+        console.log('socketIdList');
+        console.log(socketIdList);
+        console.log('socket.id in list?');
+        console.log(socketIdList.includes(socket.id));
+        if (!socketIdList.includes(socket.id)) {
+          setSocketIdList(socketIdList.concat(socket.id));
+          console.log('should soon be ');
+          console.log(socketIdList.concat(socket.id));
+        }
+
+        if (socket.id != socketIdList[0] && socketIdList.length > 1) {
+          socket.disconnect();
+        }
+      }, 1000);
+
+      socket.on('connect', () => {
+        console.log('connected');
+      });
+      socket.on('playerPos', (data) => {
+        console.log(data);
+        setPlayerPos(data);
+      });
+      socket.on('gameData', (data) => {
+        console.log('gameData');
+        console.log(data);
+      });
+      socket.on('playerDisconnected', (data) => {
+        console.log('playerDisconnected');
+        console.log(data);
+      });
+    } catch (error) {
+      console.log(error);
     }
-    fetchInitData()
-  }, [])
+
+    // Cleanup function
+    return () => {
+      socket.disconnect();
+
+      if (intervalId) {
+        clearInterval(intervalId);
+      }
+    };
+  }, []);
 
   return (
     <APIContext.Provider value={{ apiClient }}>
       <p>{exampleText}</p>
       <p>server url: {serverurl}</p>
+      <div style={{ transform: `translate(${playerPos.x}px, ${playerPos.y}px)` }} className='playerBlob'>
+
+      </div>
     </APIContext.Provider>
   )
 }
